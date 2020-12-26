27091bccd6202f7d8d524dc0a09f76204fc30835
Artur-Pastana <reitutu_15@hotmail.com>
2020-12-25 23:26:02 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/estudojava/workshopmongo/config/Instantiation.java
+++ src/main/java/com/estudojava/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.estudojava.workshopmongo.domain.Post;
 import com.estudojava.workshopmongo.domain.User;
+import com.estudojava.workshopmongo.dto.AuthorDTO;
 import com.estudojava.workshopmongo.repository.PostRepository;
 import com.estudojava.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,13 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Ana Maria", "maria@gmail.com");
 		User anne = new User(null, "Anne Caroline", "anne@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "vou viajar para o matapi, Abracos",maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feli hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(iris, maria, anne));
 		//userRepository.save(Arrays.asList(iris, maria, anne)); da erro
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "vou viajar para o matapi, Abracos", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feli hoje!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		//postRepository.save(Arrays.asList(post1, post2));da erro usando save
 	}
