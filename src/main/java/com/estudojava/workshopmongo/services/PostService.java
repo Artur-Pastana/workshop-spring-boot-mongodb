0efa95874dad89de0e6a97dce7cbd89d97767aea
Artur-Pastana <reitutu_15@hotmail.com>
2021-01-02 21:22:48 -0300

    Consulta com vários critérios

--- src/main/java/com/estudojava/workshopmongo/services/PostService.java
+++ src/main/java/com/estudojava/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.estudojava.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,9 @@ public class PostService {
 		//return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 *100);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 	
 }
